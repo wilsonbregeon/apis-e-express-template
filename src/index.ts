788d7d6236eac10ae4ffa8fe1f6f60dadbc4eecb
yuzo <yuzokamoto@gmail.com>
2023-01-04 13:38:07 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -6,27 +6,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(3003, () => {
-  console.log('Server running in http://localhost:3003')
+    console.log("Servidor rodando na porta 3003")
+})
+
+app.get('/ping', (req: Request, res: Response) => {
+    res.send('Pong!')
 })
